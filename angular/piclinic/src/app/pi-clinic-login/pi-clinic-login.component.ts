3d0145ca9698274e14a684df62d0f0cb9528074a
Bob Watson <bobwatson@google.com>
2022-08-12 11:42:12 -0700

    disable dashboard navigation for testing

--- angular/piclinic/src/app/pi-clinic-login/pi-clinic-login.component.ts
+++ angular/piclinic/src/app/pi-clinic-login/pi-clinic-login.component.ts
@@ -36,7 +36,7 @@ export class PiClinicLoginComponent implements OnInit {
 
   loginSuccess(data: currentSessionInfo): void {
     this.currentSession = data;
-    this.router.navigate(['clinicDash']);
+    // this.router.navigate(['clinicDash']);
   }
 
   loginUserError(err: HttpErrorResponse): void {
